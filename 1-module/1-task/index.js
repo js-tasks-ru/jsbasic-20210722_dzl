7a760a88b370102e11e0fded4e14b530f56bd337
Dzmitry Liashko <dzliashko@gmail.com>
2021-07-27 19:55:32 +0300

    task 1-1 finished

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
 function factorial(n) {
   // ваш код...
+  let fact = 1;
+  if (n === 0 || n === 1) {
+    fact = 1;
+  } else {
+    for (let i = n; i > 0; i--) {
+      fact *= i;
+    }
+  }
+  return fact;
 }
