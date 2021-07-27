29e573e722d667da5b4f64a75640fb16f69b4ac2
Dzmitry Liashko <dzliashko@gmail.com>
2021-07-27 20:13:52 +0300

    task 1-3 finished

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,12 @@
 function ucFirst(str) {
   // ваш код...
+  let result;
+  if (str === '') {
+    result = '';
+  } else if (str.length == 1) {
+    result = str.toUpperCase();
+  } else {
+    result = str[0].toUpperCase() + str.slice(1);
+  }
+  return result;
 }
