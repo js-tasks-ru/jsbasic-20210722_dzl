c33f4af74086a1a036f353b65dc4faa4260af12d
Dzmitry Liashko <dzliashko@gmail.com>
2021-08-11 20:29:18 +0300

    task 3-5 finished

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,10 @@
 function getMinMax(str) {
   // ваш код...
+  const arr = str.split(' ').filter(Number).sort((a, b) => a - b);
+  const result = {
+    'min': Number(arr[0]),
+    'max': Number(arr[arr.length - 1]),
+  };
+  return result;
 }
+
