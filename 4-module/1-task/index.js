7d6387556a021cf66fea5188168d11c9990edbfa
diniles <dzliashko@gmail.com>
2021-08-19 14:18:09 +0300

    Task 4-1 finished

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,10 @@
 function makeFriendsList(friends) {
   // ваш код...
+  let ul = document.createElement('ul');
+  for (const elem of friends) {
+    let li = document.createElement('li');
+    li.textContent = elem.firstName + ' ' + elem.lastName;
+    ul.append(li);
+  }
+  return ul;
 }
