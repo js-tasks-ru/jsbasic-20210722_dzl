1a1e396bf2e704555940ed53e4dfe0655df9c821
diniles <dzliashko@gmail.com>
2021-08-19 15:19:40 +0300

    Task 4-3 finished

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,21 @@
 function highlight(table) {
   // ваш код...
+  for (let i = 1; i < table.rows.length; i++) {
+    let row = table.rows[i];
+    if (row.cells[1].innerHTML < 18) {
+      row.setAttribute('style', 'text-decoration: line-through');
+    }
+    if (row.cells[2].innerHTML === 'm') {
+      row.classList.add('male');
+    } else {
+      row.classList.add('female');
+    }
+    if (row.cells[3].getAttribute('data-available') === 'true') {
+      row.classList.add('available');
+    } else if (row.cells[3].getAttribute('data-available') === 'false') {
+      row.classList.add('unavailable');
+    } else {
+      row.setAttribute('hidden', 'true');
+    }
+  }
 }
