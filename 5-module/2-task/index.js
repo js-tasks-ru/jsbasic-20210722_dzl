40a6e9963b1c48db5eb6cd61fa53b432e02a0799
Dzmitry Liashko <dzliashko@gmail.com>
2021-08-28 21:47:36 +0300

    Add: task 5-2 finished

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,8 @@
 function toggleText() {
   // ваш код...
+  document.getElementsByClassName("toggle-text-button")[0].onclick =
+    function () {
+      let elem = document.getElementById("text");
+      elem.hidden == false ? (elem.hidden = true) : (elem.hidden = false);
+    };
 }
